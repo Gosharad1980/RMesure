81889191ff3c758093ebbb75bd2315b4bda8cfc6
Moi <Moi@3eb53af06f81da0792505d7a3487544771630632>
2023-12-07 22:17:43 +0100

    On a créé notre première librairie externe :-)

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+// Entering ta_gueule_le_compilo
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_assignments)]
+// Ending ta_gueule_le_compilo
+
+
+#[derive(Debug)]
+pub struct RMesure
+{
+	pub valeur: f32,
+	pub epsilon: f32,
+    pub alpha: f32,
+}
