5a236d73cad1eef9bf87863a5ac21047534080e6
Moi <Moi@3eb53af06f81da0792505d7a3487544771630632>
2023-12-10 01:56:54 +0100

    On continu la conversion de la classe - constructeurs fait - pattern matching dans constructeur : fait - drop / clone à faire - continuer la conversion des opérateurs

--- src/lib.rs
+++ src/lib.rs
@@ -5,35 +5,88 @@
 #![allow(dead_code)]
 #![allow(unused_assignments)]
 // Ending ta_gueule_le_compilo
+//use core::f64::{EPSILON, MAX};
+//use core::f32::{EPSILON, MAX};
 
+use core::f64;
+
+pub const RMESURE_EPS: f64 = f64::EPSILON;
+pub const RMESURE_MAX: f64 = 9223371500000000000.0; //f32::MAX.sqrt()/2.0;
 
 #[derive(Debug)]
 pub struct RMesure
 {
-	pub valeur: f64,
-	pub epsilon: f64,
-    pub alpha: f64,
+	valeur: f64,
+	epsilon: f64,
+    alpha: f64,
+}
+
+impl RMesure 
+{
+    pub fn new(valeur: f64, epsilon: f64, alpha: f64) -> Self 
+	{
+        Self { valeur, epsilon, alpha }
+    }
+
+	pub fn zero() -> Self 
+	{
+		Self { valeur: 0.0, epsilon: RMESURE_EPS, alpha: 95.45 }
+    }
+
+	pub fn scalaire(valeur: f64) -> Self
+	{
+		Self { valeur, epsilon: RMESURE_EPS, alpha: 95.45 }
+	}
+
+
+	pub fn loi(valeur: f64, it: f64, loi: char) -> Self 
+	{
+		// Dans le cadre de mesures effectuées dans des conditions bien identifiées,
+		// il est possible d'estimer l'incertitude type directement à partir de
+		// l'intervalle de tolérance à l'aide des lois suivante
+		//
+		//		1) 'R' : Résolution d'un indicateur numérique       : epsilon = it / rac(12.0)
+		//		2) 'H' : Hystérésis tel que it = MAXI - MINI        : epsilon = it / rac(12.0)
+		//		3) 'S' : évolution Sinusoïdale sur it = MAXI - MINI : epsilon = it / 1.4
+		//		4) 'N' : loi Normale par défaut, K = 2              : epsilon = it / 2.0
+		//		5) 'C' : appareil de Classe +/- it                  : epsilon = it / rac(3.0)
+
+		let mut inner_epsilon: f64 = 0.0;
+
+		match loi
+		{
+			'R' => inner_epsilon = f64::abs(it) / f64::sqrt(12.0),
+			'H' => inner_epsilon = f64::abs(it) / f64::sqrt(12.0),
+			'S' => inner_epsilon = f64::abs(it) / 1.4,
+			'C' => inner_epsilon = f64::abs(it) / f64::sqrt(3.0),
+			// c'est la loi par défaut dans tout bon certificat d'étalonnage qui se respecte
+			'N' => inner_epsilon = f64::abs(it) / 2.0, 
+			_ => inner_epsilon = f64::abs(it) / 2.0, 
+		}
+		
+		Self { valeur, epsilon: inner_epsilon, alpha: 95.45 }
+    }	
+	
 }
 
 impl RMesure
-{
-	
+{	
 	pub fn Val(&self) -> f64	{ self.valeur 	}	// LA mesure en cours de traitement
 	pub fn Alpha(&self) -> f64	{ self.alpha 	}	// Taux de confiance
 	pub fn Eps(&self) -> f64 	{ self.epsilon	}	// Incertitude type.
-	pub fn IT(&self) -> f64 	{ self.epsilon * self.K() }	// Intervalle de tol�rance = Eps x K
+	pub fn IT(&self) -> f64 	{ self.epsilon * self.K() }	// Intervalle de tolérance = Eps x K
 
 	// Coeff d'élargissement
 	fn K(&self) -> f64 
 	{
-		// Calcul par interpolation du coeff d'�largissement � l'aide
-		// des valeurs d�crites dans la norme "NF ENV 13005"
+		// Calcul par interpolation du coeff d'élargissement à l'aide
+		// des valeurs décrites dans la norme "NF ENV 13005"
 		let p: [f64; 8] = [99.95 , 99.73 , 99.00 , 95.45 , 95.00 , 90.00 , 68.27 , 0.000];
 		let k: [f64; 8] = [4.000 , 3.000 , 2.576 , 2.000 , 1.960 , 1.645 , 1.000 , 0.000];
 
 		let mut a: f64 = 0.0;
 		let mut b: f64 = 0.0;
-		let mut i: usize = 0;
+		let mut i = 0;
 
 		// Recherche du cadran dans lequel on se situe
 		for j in 1..p.len()
