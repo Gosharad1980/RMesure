935eaed4ba1bd21335a9813bb09a6e8aeee5b219
Gosharad1980 <199524053+Gosharad1980@users.noreply.github.com>
2025-12-08 22:47:50 +0100

    UPDATE : test_math.rs
    sinh
    cosh
    tanh
    asinh
    acosh
    atanh

--- tests/test_math.rs
+++ tests/test_math.rs
@@ -40,7 +40,6 @@ mod tests
 	#[test]
 	fn test_sin() 
 	{
-		let mesure:		RMesure = RMesure::new(1.570796_f64, 0.001_f64 , 95.45_f64);
 		let resultat:	RMesure = RMesure::new(1.570796_f64, 0.001_f64 , 95.45_f64).sin();
 
 		let resultat_val = 1.570796_f64.sin();
@@ -53,7 +52,6 @@ mod tests
 	#[test]
 	fn test_cos() 
 	{
-		let mesure:		RMesure = RMesure::new(1.570796_f64, 0.001_f64 , 95.45_f64);
 		let resultat:	RMesure = RMesure::new(1.570796_f64, 0.001_f64 , 95.45_f64).cos();
 
 		let resultat_val = 1.570796_f64.cos();
@@ -66,7 +64,6 @@ mod tests
 	#[test]
 	fn test_tan() 
 	{
-		let mesure:		RMesure = RMesure::new(1.570796_f64, 0.001_f64 , 95.45_f64);
 		let resultat:	RMesure = RMesure::new(1.570796_f64, 0.001_f64 , 95.45_f64).tan();
 
 		let resultat_val = 1.570796_f64.tan();
@@ -79,7 +76,6 @@ mod tests
 	#[test]
 	fn test_asin() 
 	{
-		let mesure:		RMesure = RMesure::new(2.0_f64.sqrt() / 2.0_f64, 0.001_f64 , 95.45_f64);
 		let resultat:	RMesure = RMesure::new(2.0_f64.sqrt() / 2.0_f64, 0.001_f64 , 95.45_f64).asin();
 
 		let resultat_val = (2.0_f64.sqrt() / 2.0_f64).asin();
@@ -92,7 +88,6 @@ mod tests
 	#[test]
 	fn test_acos() 
 	{
-		let mesure:		RMesure = RMesure::new(2.0_f64.sqrt() / 2.0_f64, 0.001_f64 , 95.45_f64);
 		let resultat:	RMesure = RMesure::new(2.0_f64.sqrt() / 2.0_f64, 0.001_f64 , 95.45_f64).acos();
 
 		let resultat_val = (2.0_f64.sqrt() / 2.0_f64).acos();
@@ -105,11 +100,82 @@ mod tests
 	#[test]
 	fn test_atan() 
 	{
-		let mesure:		RMesure = RMesure::new(2.0_f64.sqrt() / 2.0_f64, 0.001_f64 , 95.45_f64);
 		let resultat:	RMesure = RMesure::new(2.0_f64.sqrt() / 2.0_f64, 0.001_f64 , 95.45_f64).atan();
 
 		let resultat_val = (2.0_f64.sqrt() / 2.0_f64).atan();
 		let resultat_eps = ((1.0_f64 - (2.0_f64.sqrt() / 2.0_f64)).recip() * 0.001_f64.powf(2.0_f64)).sqrt();
+		let resultat_alf = 95.45_f64;
+
+		let resultat = resultat.Val() == resultat_val && resultat.Eps() == resultat_eps && resultat.Alpha() == resultat_alf;    
+	}
+
+	#[test]
+	fn test_sinh() 
+	{
+		let resultat:	RMesure = RMesure::new(1.570796_f64, 0.001_f64 , 95.45_f64).sinh();
+
+		let resultat_val = 1.570796_f64.sinh();
+		let resultat_eps = (1.570796_f64.cosh().powf(2.0_f64) * 0.001_f64.powf(2.0_f64)).sqrt();
+		let resultat_alf = 95.45_f64;
+
+		let resultat = resultat.Val() == resultat_val && resultat.Eps() == resultat_eps && resultat.Alpha() == resultat_alf;    
+	}
+	
+	#[test]
+	fn test_cosh() 
+	{
+		let resultat:	RMesure = RMesure::new(1.570796_f64, 0.001_f64 , 95.45_f64).cosh();
+
+		let resultat_val = 1.570796_f64.cosh();
+		let resultat_eps = (1.570796_f64.sinh().powf(2.0_f64) * 0.001_f64.powf(2.0_f64)).sqrt();
+		let resultat_alf = 95.45_f64;
+
+		let resultat = resultat.Val() == resultat_val && resultat.Eps() == resultat_eps && resultat.Alpha() == resultat_alf;    
+	}
+
+	#[test]
+	fn test_tanh() 
+	{
+		let resultat:	RMesure = RMesure::new(1.570796_f64, 0.001_f64 , 95.45_f64).tanh();
+
+		let resultat_val = 1.570796_f64.tanh();
+		let resultat_eps = ((1.0_f64 + 1.570796_f64.tanh().powf(2.0_f64)).powf(2.0_f64) * 0.001_f64.powf(2.0_f64)).sqrt();
+		let resultat_alf = 95.45_f64;
+
+		let resultat = resultat.Val() == resultat_val && resultat.Eps() == resultat_eps && resultat.Alpha() == resultat_alf;    
+	}
+
+	#[test]
+	fn test_asinh() 
+	{
+		let resultat:	RMesure = RMesure::new(2.0_f64.sqrt() / 2.0_f64, 0.001_f64 , 95.45_f64).asinh();
+
+		let resultat_val = (2.0_f64.sqrt() / 2.0_f64).asinh();
+		let resultat_eps = ((2.0_f64.sqrt() / 2.0_f64).powf(2.0_f64) + 1.0_f64).recip() * 0.001_f64.powf(2.0_f64);
+		let resultat_alf = 95.45_f64;
+
+		let resultat = resultat.Val() == resultat_val && resultat.Eps() == resultat_eps && resultat.Alpha() == resultat_alf;    
+	}
+
+	#[test]
+	fn test_acosh() 
+	{
+		let resultat:	RMesure = RMesure::new(2.0_f64.sqrt() / 2.0_f64, 0.001_f64 , 95.45_f64).acosh();
+
+		let resultat_val = (2.0_f64.sqrt() / 2.0_f64).acosh();
+		let resultat_eps = ((2.0_f64.sqrt() / 2.0_f64).powf(2.0_f64) - 1.0_f64).recip() * 0.001_f64.powf(2.0_f64);
+		let resultat_alf = 95.45_f64;
+
+		let resultat = resultat.Val() == resultat_val && resultat.Eps() == resultat_eps && resultat.Alpha() == resultat_alf;    
+	}
+
+	#[test]
+	fn test_atanh() 
+	{
+		let resultat:	RMesure = RMesure::new(2.0_f64.sqrt() / 2.0_f64, 0.001_f64 , 95.45_f64).atanh();
+
+		let resultat_val = (2.0_f64.sqrt() / 2.0_f64).atanh();
+		let resultat_eps = (1.0_f64 - (2.0_f64.sqrt() / 2.0_f64).powf(2.0_f64)).recip().powf(2.0_f64) * 0.001_f64.powf(2.0_f64);
 		let resultat_alf = 95.45_f64;
 
 		let resultat = resultat.Val() == resultat_val && resultat.Eps() == resultat_eps && resultat.Alpha() == resultat_alf;    
